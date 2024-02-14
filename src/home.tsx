a6ccfd1fb9e50ef61e28307af3756c43151716d2
Mohit Kamal <kamalmohit2111@gmail.com>
2024-02-14 17:10:01 +0530

    Removing redundant loading screen

--- src/home.tsx
+++ src/home.tsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "preact/hooks";
-import { TailSpin } from "react-loader-spinner";
+import { useEffect } from "preact/hooks";
 import { useNavigate } from "react-router-dom";
 const searchParams = import.meta.env.DEV
   ? "ewogICAgImdhbWVUeXBlIjogIkNMQVNTSUMiLAogICAgImdhbWVOYW1lIjogIkJhdHRsZWNpdHkiLAogICAgImdhbWVJRCI6ICI2NWM5MmI2YTdiYmY0MjhjMjUxMzg3NTYiLAogICAgIm1vZGUiOiAwLAogICAgImJldFBsYWNlZCI6IDAsCiAgICAicGxheWVycyI6IFsKICAgICAgICB7CiAgICAgICAgICAgICJ1c2VybmFtZSI6ICJEaHJ1djcwIiwKICAgICAgICAgICAgIl9pZCI6ICI2NWI5ZjQ1ZTI4NjdjMTI5ZDI2MThmNTMiLAogICAgICAgICAgICAiYXZhdGFyIjogIm5pbCIKICAgICAgICB9CiAgICBdLAogICAgInJlc3VsdHMiOiBbXSwKICAgICJhZGRpdGlvbmFsIjoge30sCiAgICAiY2hhbGxlbmdlcyI6IHt9Cn0="
   : new URLSearchParams(window.location.search)?.get("data");
 function HomePage() {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const navigate = useNavigate();
   useEffect(() => {
     if (searchParams) {
-      setIsLoaded(true);
-      setTimeout(() => {
-        navigate(`/singleplayer?data=${searchParams}`);
-        setIsLoaded(false);
-      }, 3000);
+      navigate(`/singleplayer?data=${searchParams}`);
     }
   }, [searchParams]);
 
-  if (!searchParams && !isLoaded) {
+  if (!searchParams) {
     return (
       <p className="text-white align-center">
         Something went wrong loading the game.
@@ -25,22 +19,7 @@ function HomePage() {
     );
   }
 
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="flex flex-col gap-1 items-center">
-        <TailSpin
-          visible={true}
-          height="80"
-          width="80"
-          color="#ffffff"
-          ariaLabel="tail-spin-loading"
-          radius="3"
-        />
-        <h2 className="text-white text-xl">Battlecity</h2>
-        <h2 className="text-white text-md">Loading</h2>
-      </div>
-    </div>
-  );
+  return null;
 }
 
 export default HomePage;

5139813d090d216b7d6d0f5bedc1d1381e500d40
Mohit Kamal <kamalmohit2111@gmail.com>
2024-02-23 11:34:50 +0530

    Board css fix

--- src/helper/SIZES.ts
+++ src/helper/SIZES.ts
@@ -1,14 +1,19 @@
 function calculateCellStyle() {
-  let _cellStyle = `grid grid-cols-[repeat(9,40px)]`;
-  if (window.matchMedia("(max-device-width : 475px)").matches) {
-    _cellStyle = `grid grid-cols-[repeat(9,40px)]`;
+  let _cellStyle = `grid grid-cols-[repeat(9,35px)]`;
+  if (window.matchMedia("(max-device-width : 375px)").matches) {
+    _cellStyle = `grid grid-cols-[repeat(9,32px)]`;
+  }
+  if (
+    window.matchMedia("(min-device-width : 376px) && (max-device-width: 475px)")
+      .matches
+  ) {
+    _cellStyle = `grid grid-cols-[repeat(9,35px)]`;
   }
   if (
     window.matchMedia(
       "(min-device-width : 476px) && (max-device-width : 768px)"
     ).matches
   ) {
-    console.log("ran");
     _cellStyle = `grid grid-cols-[repeat(9,46px)]`;
   }
   if (window.matchMedia("(min-device-width : 769px)").matches) {
@@ -17,10 +22,15 @@ function calculateCellStyle() {
   return _cellStyle;
 }
 function calculateCellSize() {
-  let _cellSize = 37;
+  let _cellSize = 32;
   if (window.matchMedia("(max-device-width : 475px)").matches) {
-    console.log("ran");
-    _cellSize = 40;
+    _cellSize = 32;
+  }
+  if (
+    window.matchMedia("(min-device-width : 376px) && (max-device-width: 475px)")
+      .matches
+  ) {
+    _cellSize = 35;
   }
   if (
     window.matchMedia("(min-device-width : 476px) && (max-device-width: 768px)")
@@ -35,18 +45,25 @@ function calculateCellSize() {
   return _cellSize;
 }
 function calculateShipsContainer() {
-  let _cellStyle = `max-w-[500px]`;
-  if (window.matchMedia("(min-device-width : 600px)").matches) {
-    _cellStyle = `max-w-[500px]`;
+  let _cellStyle = `max-w-[320px]`;
+  if (window.matchMedia("(max-device-width : 375px)").matches) {
+    _cellStyle = `max-w-[320px]`;
   }
-  if (window.matchMedia("(min-device-width : 1024px)").matches) {
+  if (
+    window.matchMedia("(min-device-width : 376px) && (max-device-width: 475px)")
+      .matches
+  ) {
+    _cellStyle = `max-w-[350px]`;
+  }
+  if (
+    window.matchMedia(
+      "(min-device-width : 476px) && (max-device-width : 768px)"
+    ).matches
+  ) {
+    _cellStyle = `max-w-[460px]`;
+  }
+  if (window.matchMedia("(min-device-width : 769px)").matches) {
     _cellStyle = `max-w-[600px]`;
-  }
-  if (window.matchMedia("(max-device-width : 460px)").matches) {
-    _cellStyle = `max-w-[420px]`;
-  }
-  if (window.matchMedia("(max-device-width : 390px)").matches) {
-    _cellStyle = `max-w-[370px]`;
   }
   return _cellStyle;
 }
